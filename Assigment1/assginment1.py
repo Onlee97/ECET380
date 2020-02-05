aa43c8d6213629661daffa46ea902a2e04e6a605
Onlee97 <lekhacduy97@gmail.com>
2020-02-04 22:23:55 -0500

    MLP nan loss

--- Assigment1/assginment1.py
+++ Assigment1/assginment1.py
@@ -1,42 +1,45 @@
 import pickle
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.base import clone
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 import numpy as np
-
+import tensorflow as tf 
+from tensorflow import keras
 
 #Prepare the data
 data = pd.read_pickle('appml-assignment1-dataset.pkl')
 y = data['y']
-X = data['X'][['CAD-open', 'CAD-high', 'CAD-low', 'CAD-close']]
-X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, test_size = 0.3)
+X = data['X'].drop("date", axis = 1)
+X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, test_size = 0.4)
 X_train, X_valid, y_train, y_valid = train_test_split(X_train_full, y_train_full)
 
-print(X_train)
 
-# prepare the data
-poly_scaler = Pipeline([("std_scaler", StandardScaler())])
+# Normalize the data
+scaler = Pipeline([("std_scaler", StandardScaler())])
+X_train_scaled = scaler.fit_transform(X_train)
+X_valid_scaled = scaler.fit_transform(X_valid)
+X_test_scaled = scaler.fit_transform(X_test)
 
+print(X_train_scaled.shape[1:])
 
-X_train_poly_scaled = poly_scaler.fit_transform(X_train)
-X_val_poly_scaled = poly_scaler.transform(X_valid)
+model = keras.models.Sequential([
+	keras.layers.Dense(30, activation="relu", input_shape=X_train_scaled.shape[1:]),
+	keras.layers.Dense(30, activation="relu"),
+	keras.layers.Dense(1)
+	])
 
-from sklearn.linear_model import SGDRegressor
-sgd_reg = SGDRegressor(max_iter=1, tol=-np.infty, warm_start=True,
-						penalty=None, learning_rate="constant", eta0=0.0005)
-						# warm_start=True means when fit is called again, continues training where left off
-minimum_val_error = float("inf")
-best_epoch = None
-best_model = None
-for epoch in range(10):
-	sgd_reg.fit(X_train_poly_scaled, y_train) # continues where it left off
-	y_val_predict = sgd_reg.predict(X_val_poly_scaled)
-	val_error = mean_squared_error(y_valid, y_val_predict)
-	print("mean_squared_error: ", val_error)
-	if val_error < minimum_val_error:
-		minimum_val_error = val_error
-		best_epoch = epoch
-		best_model = clone(sgd_reg)
+model.compile(loss="mean_squared_error", optimizer="sgd")
+
+early_stopping_cb = keras.callbacks.EarlyStopping(patience=10, restore_best_weights=True)
+
+history = model.fit(X_train_scaled, y_train, epochs=100,
+					validation_data= (X_valid_scaled, y_valid),
+					callbacks=[early_stopping_cb]
+					)
+
+mse_test = model.evaluate(X_test_scaled, y_test)
+y_pred = model.predict(X_test_scaled)
