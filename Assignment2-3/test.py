b92f1228f5e5d0438198a1a4cec33a3b3048f8ed
Onlee97 <lekhacduy97@gmail.com>
2020-03-04 21:17:07 -0500

    model 86% accuracy

---
+++ Assignment2-3/test.py
@@ -0,0 +1,15 @@
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets as tfds
+from tensorflow import keras
+from preprocessDefinition import preprocess
+
+model=tf.keras.models.load_model('flowersModel.h5')
+evalset,info = tfds.load(name='oxford_flowers102', split='test',as_supervised=True,with_info=True)
+evalPipe=evalset.map(preprocess,num_parallel_calls=16).batch(128).prefetch(1)
+for feats,lab in evalPipe.unbatch().batch(6000).take(1):
+	probPreds=model.predict(feats)
+
+top1err=tf.reduce_mean(keras.metrics.sparse_top_k_categorical_accuracy(lab,probPreds,k=1))
+top5err=tf.reduce_mean(keras.metrics.sparse_top_k_categorical_accuracy(lab,probPreds,k=5))
+top10err=tf.reduce_mean(keras.metrics.sparse_top_k_categorical_accuracy(lab,probPreds,k=10))
+print(top10err, top5err, top1err)
